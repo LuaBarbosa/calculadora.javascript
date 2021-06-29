cc625b16d261c9b89759d81dbdb2b95a1d984aed
LuaBarbosa <lua.barbosa@yahoo.com.br>
2021-06-29 11:16:28 -0300

    Feat: Adicionando pasta Controller

---
+++ Api/controllers/apiControllers.js
@@ -0,0 +1,26 @@
+exports.test = function (req, res) {
+  res.send("Olá! Teste ao Controller");
+};
+
+exports.listar = function (req, res) {
+  res.send({ type: "GET" });
+};
+
+exports.criar = function (req, res) {
+  res.send({
+    type: "POST",
+    Anuncio: req.body.anuncio,
+    Cliente: req.body.Cliente,
+    Inicio: req.body.inicio,
+    Termino: req.body.Termino,
+    Investimento: req.body.investimento,
+  });
+};
+
+exports.atualizar = function (req, res) {
+  res.send({ type: "PUT" });
+};
+
+exports.delete = function (req, res) {
+  res.send({ type: "DELETE" });
+};
